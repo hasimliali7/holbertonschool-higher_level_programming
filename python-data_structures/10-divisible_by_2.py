2e979aa5f3b9e29e3a7451a2ee936dcea39ff350
hasimliali7 <hasimliali07@gmail.com>
2026-02-14 11:47:17 +0000

    Fix SyntaxError and style in Task 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    # Nəticələri yığmaq üçün yeni siyahı yaradırıq
-    result_list = []
-    
+    """Finds all multiples of 2 in a list"""
+    new_list = []
     for num in my_list:
         if num 
