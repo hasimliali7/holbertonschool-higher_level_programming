53ededfc61cf3b105cbeab514aec54c0ac502d72
hasimliali7 <hasimliali07@gmail.com>
2026-02-14 11:10:48 +0000

    Task 3: Infinite addition of arguments

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    total = 0
+    # sys.argv[0] skriptin adı olduğu üçün 1-ci indeksdən başlayırıq
+    for i in range(1, len(sys.argv)):
+        total += int(sys.argv[i])
+    print("{:d}".format(total))
