ff840f9f1f5cdf451999ebdd67eac7700efc0593
hasimliali7 <hasimliali7@gmail.com>
2026-02-16 12:47:26 +0000

    Task 3: Implement print_square with TDD and specific error handling

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module provides a function that prints a square with #.
+The size of the square is validated to be a positive integer.
+"""
+
+
+def print_square(size):
+    """
+    Prints a square with the character #.
+
+    Args:
+        size: The size length of the square (int).
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+    """
+    if not isinstance(size, int):
+        if isinstance(size, float) and size < 0:
+            raise TypeError("size must be an integer")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
