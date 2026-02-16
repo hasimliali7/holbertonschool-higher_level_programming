c413475e9e091cbb76fc9205d241f2e2aad624a9
hasimliali7 <hasimliali7@gmail.com>
2026-02-16 12:42:32 +0000

    Task 0: Handle NaN and Infinity edge cases

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -18,11 +18,17 @@ def add_integer(a, b=98):
         The sum of a and b as an integer.
 
     Raises:
-        TypeError: If a or b are not integers or floats.
+        TypeError: If a or b are not integers, floats, or are NaN/Inf.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    # NaN və Infinity yoxlanışı (Overflow qarşısını almaq üçün)
+    if a != a or abs(a) > 1.7976931348623157e+308:
+        raise TypeError("a must be an integer")
+    if b != b or abs(b) > 1.7976931348623157e+308:
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
