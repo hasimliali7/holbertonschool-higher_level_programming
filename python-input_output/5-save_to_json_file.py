0eb1ef1885d237995d4bea17997acd80364bb570
hasimliali7 <hasimliali07@gmail.com>
2026-03-03 13:15:14 +0000

    Add task 5: save_to_json_file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Bu modul Python obyektini JSON formatında fayla yazan funksiyanı ehtiva edir.
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Obyektin JSON təmsilini (representation) mətn faylına yazır.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
