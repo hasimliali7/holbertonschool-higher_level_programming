90184ebe82ae81d8eb92113d4ed5ee27a45d25a4
hasimliali7 <hasimliali07@gmail.com>
2026-03-03 13:07:19 +0000

    Move task 3 to correct directory

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -8,11 +8,5 @@ import json
 def to_json_string(my_obj):
     """
     Python obyektinin JSON təmsilini (string) qaytarır.
-
-    Args:
-        my_obj: Serializasiya olunacaq Python obyekti.
-
-    Returns:
-        str: Obyektin JSON string forması.
     """
     return json.dumps(my_obj)
