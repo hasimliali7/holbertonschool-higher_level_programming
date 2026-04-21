b3d05bf63edddb532e3d3aa098b25b65d3092578
hasimliali7 <hasimliali07@gmail.com>
2026-04-21 10:16:32 +0000

    Fix Task 11: Add Louisiana and commit correctly

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
 Adds the State object 'Louisiana' to the database hbtn_0e_6_usa.
+Prints the new state's id after creation.
 """
 import sys
 from model_state import Base, State
@@ -9,16 +10,26 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
+    # Verilənlər bazasına qoşulmaq üçün engine yaradırıq
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+    
+    # Session (sessiya) yaradırıq
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    # Yeni State obyekti yaradırıq
     new_state = State(name="Louisiana")
+    
+    # Obyekti sessiyaya əlavə edirik
     session.add(new_state)
+    
+    # Dəyişikliyi bazada təsdiqləyirik (COMMIT)
     session.commit()
 
+    # Yeni yaranan ID-ni çap edirik
     print("{}".format(new_state.id))
 
+    # Sessiyanı bağlayırıq
     session.close()
