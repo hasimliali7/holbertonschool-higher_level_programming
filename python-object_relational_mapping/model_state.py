177b6cfe492cd57c61e760c41ea59e4d0ce544c9
hasimliali7 <hasimliali07@gmail.com>
2026-04-21 10:11:45 +0000

    Complete all tasks including relationships

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""
-Contains the class definition of a State and an instance Base.
-"""
+"""State model with relationship to City."""
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """
-    State class that inherits from Base.
-    Maps to the MySQL table 'states'.
-    """
+    """State class with relationship."""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
